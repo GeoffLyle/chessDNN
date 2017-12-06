7fb8c96642126ff9388f1568f0d000419811c7da
MeltingShoe <meltingshoe@gmail.com>
2017-12-06 09:12:03 -0800

    Formatting
    Pretty sure all that's changed is formatting after using a linter, and removing the network test code that didn't work

--- chess-dnn.py
+++ chess-dnn.py
@@ -1,7 +1,6 @@
 import chess
 import numpy as np
 import time
-import tensorflow as tf
 import pickle
 import random
 from random import shuffle
@@ -16,65 +15,66 @@ board.push_san("Bc4")
 
 print(board)
 
+
 def parseBoard(isWhite):
-    b0 = np.zeros((12,8,8))
+    b0 = np.zeros((12, 8, 8))
     i = 0
-    while(i<8):
+    while(i < 8):
         j = 0
-        while(j<8):
-            k = str(board.piece_at(i*8+j))
-            if(isWhite==True):
-                if(k=="P"):
+        while(j < 8):
+            k = str(board.piece_at(i * 8 + j))
+            if isWhite is True:
+                if(k == "P"):
                     b0[0][i][j] = 1
-                elif(k=="B"):
+                elif(k == "B"):
                     b0[1][i][j] = 1
-                elif(k=="N"):
+                elif(k == "N"):
                     b0[2][i][j] = 1
-                elif(k=="R"):
+                elif(k == "R"):
                     b0[3][i][j] = 1
-                elif(k=="Q"):
+                elif(k == "Q"):
                     b0[4][i][j] = 1
-                elif(k=="K"):
+                elif(k == "K"):
                     b0[5][i][j] = 1
-                elif(k=="p"):
+                elif(k == "p"):
                     b0[6][i][j] = 1
-                elif(k=="b"):
+                elif(k == "b"):
                     b0[7][i][j] = 1
-                elif(k=="n"):
+                elif(k == "n"):
                     b0[8][i][j] = 1
-                elif(k=="r"):
+                elif(k == "r"):
                     b0[9][i][j] = 1
-                elif(k=="q"):
+                elif(k == "q"):
                     b0[10][i][j] = 1
-                elif(k=="k"):
+                elif(k == "k"):
                     b0[11][i][j] = 1
             else:
-                if(k=="P"):
+                if(k == "P"):
                     b0[6][i][j] = 1
-                elif(k=="B"):
+                elif(k == "B"):
                     b0[7][i][j] = 1
-                elif(k=="N"):
+                elif(k == "N"):
                     b0[8][i][j] = 1
-                elif(k=="R"):
+                elif(k == "R"):
                     b0[9][i][j] = 1
-                elif(k=="Q"):
+                elif(k == "Q"):
                     b0[10][i][j] = 1
-                elif(k=="K"):
+                elif(k == "K"):
                     b0[11][i][j] = 1
-                elif(k=="p"):
+                elif(k == "p"):
                     b0[0][i][j] = 1
-                elif(k=="b"):
+                elif(k == "b"):
                     b0[1][i][j] = 1
-                elif(k=="n"):
+                elif(k == "n"):
                     b0[2][i][j] = 1
-                elif(k=="r"):
+                elif(k == "r"):
                     b0[3][i][j] = 1
-                elif(k=="q"):
+                elif(k == "q"):
                     b0[4][i][j] = 1
-                elif(k=="k"):
+                elif(k == "k"):
                     b0[5][i][j] = 1
-            j = j + 1
-        i = i +1
+            j += 1
+        i += 1
     return b0
 
 
@@ -82,6 +82,7 @@ def getBoard():
     a = parseBoard(board.turn)
     a = a.flatten()
     return a
+
 
 def getLegalMoveList():
     a = list(enumerate(board.legal_moves))
@@ -92,12 +93,8 @@ def getLegalMoveList():
         c.append(str(b[i]))
         i += 1
     return c
-'''
-functional programming you retard
-rewrite this as playmove, parse, save, pop, return
-that way it's a finite equation and can't fuck up something else
-'''
-    
+
+
 def playLegalMove(iteration):
     a = getLegalMoveList()
     b = len(a)
@@ -105,30 +102,32 @@ def playLegalMove(iteration):
         return False
     board.push_uci(a[iteration])
     return True
-    
+
+
 def parseMove(iteration):
     a = playLegalMove(iteration)
-    if(a == False):
+    if a is False:
         return False
     b = getBoard()
     board.pop()
     return b
 
 
+def sigmoid(x, deriv=False):
+    if(deriv):
+        return x * (1 - x)
+    return 1 / (1 + np.exp(-x))
 
-def sigmoid(x,deriv=False):
-    if(deriv==True):
-        return x*(1-x)
-    return 1/(1+np.exp(-x))
 
 def initWeights(shape):
     w = []
     shapeLen = len(shape)
     i = 1
     while(i < shapeLen):
-        w.append(np.random.randn(shape[i-1], shape[i]))
+        w.append(np.random.randn(shape[i - 1], shape[i]))
         i += 1
     return w
+
 
 def forwardPass(data, weights):
     net = data
@@ -140,9 +139,10 @@ def forwardPass(data, weights):
     net = sigmoid(net)
     net = np.dot(net, weights[3])
     net = sigmoid(net)
-    net = net[0] - net [1]
-    
+    net = net[0] - net[1]
+
     return net
+
 
 def layerOutputPass(data, weights):
     out = []
@@ -154,150 +154,150 @@ def layerOutputPass(data, weights):
     h3a = sigmoid(h3)
     ol = np.dot(h3a, weights[3])
     ola = sigmoid(ol)
-    #out.append(h1)
+    # out.append(h1)
     out.append(h1a)
-    #out.append(h2)
+    # out.append(h2)
     out.append(h2a)
-    #out.append(h3)
+    # out.append(h3)
     out.append(h3a)
-    #out.append(ol)
+    # out.append(ol)
     out.append(ola)
 
     return out
 
 
-
 def derivative(output):
     return output * (1 - output)
 
-def calcOError(target,output):
+
+def calcOError(target, output):
     return (target - output) * derivative(output)
 
-def calcNError(errorSignal,weight,output):
-    return (weight*errorSignal) * derivative(output)
 
-def backPropError(data,w,target):
-    outs = layerOutputPass(data,w)
+def calcNError(errorSignal, weight, output):
+    return (weight * errorSignal) * derivative(output)
+
+
+def backPropError(data, w, target):
+    outs = layerOutputPass(data, w)
     errors = []
     nodes = list(reversed(outs))
     weights = list(reversed(w))
     i = 0
     err = []
-    while(i<2):
-        calc = (calcOError(target[i],nodes[0][i]))
+    while(i < 2):
+        calc = (calcOError(target[i], nodes[0][i]))
         err.append(calc)
-        i+=1
+        i += 1
     errors.append(err)
     i = 0
     err = []
-    while(i<20):
+    while(i < 20):
         total = 0
         j = 0
-        while(j<2):
-            a = calcNError(errors[0][j],weights[0][i][j],nodes[1][i])
+        while(j < 2):
+            a = calcNError(errors[0][j], weights[0][i][j], nodes[1][i])
             total += a
-            j+=1
+            j += 1
         err.append(a)
-        i+=1
-    errors.append(err)
-
-    i = 0
-    err = []
-    while(i<100):
-        total = 0
-        j = 0
-        while(j<20):
-            a = calcNError(errors[1][j],weights[1][i][j],nodes[2][i])
-            total += a
-            j+=1
-        err.append(a)
-        i+=1
+        i += 1
     errors.append(err)
 
     i = 0
     err = []
-    while(i<200):
+    while(i < 100):
         total = 0
         j = 0
-        while(j<100):
-            a = calcNError(errors[2][j],weights[2][i][j],nodes[3][i])
+        while(j < 20):
+            a = calcNError(errors[1][j], weights[1][i][j], nodes[2][i])
             total += a
-            j+=1
+            j += 1
         err.append(a)
-        i+=1
+        i += 1
+    errors.append(err)
+
+    i = 0
+    err = []
+    while(i < 200):
+        total = 0
+        j = 0
+        while(j < 100):
+            a = calcNError(errors[2][j], weights[2][i][j], nodes[3][i])
+            total += a
+            j += 1
+        err.append(a)
+        i += 1
     errors.append(err)
 
     return errors
 
-def calcNewWeight(weight,error,inValue):
-    a = 0.1 * error * inValue
+
+def calcNewWeight(weight, error, inValue):
+    a = 0.001 * error * inValue
     return weight + a
 
-def updateWeights(error,w,data):
-    outs = layerOutputPass(data,w)
+
+def updateWeights(error, w, data):
+    outs = layerOutputPass(data, w)
     nodes = list(reversed(outs))
     weights = list(reversed(w))
     newWeights = weights
 
     i = 0
-    while(i<2):
+    while(i < 2):
         j = 0
-        while(j<20):
+        while(j < 20):
             a = weights[0][j][i]
             b = error[0][i]
             c = nodes[1][j]
-            nw = calcNewWeight(a,b,c)
+            nw = calcNewWeight(a, b, c)
             newWeights[0][j][i] = nw
-            j+=1
-        i+=1
+            j += 1
+        i += 1
     i = 0
-    while(i<20):
+    while(i < 20):
         j = 0
-        while(j<100):
+        while(j < 100):
             a = weights[1][j][i]
             b = error[1][i]
             c = nodes[2][j]
-            nw = calcNewWeight(a,b,c)
+            nw = calcNewWeight(a, b, c)
             newWeights[1][j][i] = nw
-            j+=1
-        i+=1
+            j += 1
+        i += 1
     i = 0
-    while(i<100):
+    while(i < 100):
         j = 0
-        while(j<200):
+        while(j < 200):
             a = weights[2][j][i]
             b = error[2][i]
             c = nodes[3][j]
-            nw = calcNewWeight(a,b,c)
+            nw = calcNewWeight(a, b, c)
             newWeights[2][j][i] = nw
-            j+=1
-        i+=1
+            j += 1
+        i += 1
     i = 0
-    while(i<200):
+    while(i < 200):
         j = 0
-        while(j<768):
+        while(j < 768):
             a = weights[3][j][i]
             b = error[3][i]
             c = data[j]
-            nw = calcNewWeight(a,b,c)
+            nw = calcNewWeight(a, b, c)
             newWeights[3][j][i] = nw
-            j+=1
-        i+=1
+            j += 1
+        i += 1
     w1 = list(reversed(newWeights))
     return w1
 
 
-'''
-rewrite this shit too
-this is literally retarded
-'''
 def chooseMove(w):
     evalList = []
     i = 0
     while(type(parseMove(i)) != bool):
-    
+
         data = parseMove(i)
-        out = forwardPass(data,w)
+        out = forwardPass(data, w)
         evalList.append(out)
         i += 1
 
@@ -305,12 +305,14 @@ def chooseMove(w):
     chosen = [i for i, j in enumerate(evalList) if j == m]
     return chosen
 
+
 def saveW(w):
     with open('outfile', 'wb') as fp:
         pickle.dump(w, fp)
 
+
 def loadW():
-    with open ('outfile', 'rb') as fp:
+    with open('outfile', 'rb') as fp:
         itemlist = pickle.load(fp)
     return itemlist
 
@@ -318,27 +320,24 @@ def loadW():
 def loss(predicted, target):
     return np.square(target - predicted).sum()
 
+
 def getRandomTrainingData():
     moves = []
     board.reset()
     i = 0
-    while(i<50):
-        if(board.is_game_over()==True):
+    while(i < 50):
+        if(board.is_game_over()):
             return moves
-        l = getLegalMoveList()
-        n = len(l)
-        iteration = random.randint(0,n-1)
+        legal = getLegalMoveList()
+        n = len(legal)
+        iteration = random.randint(0, n - 1)
         m = parseMove(iteration)
         moves.append(m)
         playLegalMove(iteration)
-        i+=1
+        i += 1
     return moves
-            
-            
 
-    
-#w = initWeights([768,200,100,20,2])
-w = loadW()
+
 def train(iterations):
     board.reset()
     i = 0
@@ -349,30 +348,28 @@ def train(iterations):
     whiteWins = 0
     blackWins = 0
     w = loadW()
-    while(i<iterations):
-        
+    while(i < iterations):
 
-        if(board.is_game_over()==True):
+        if(board.is_game_over()):
             games += 1
             if(board.result() == "1-0"):
-                whiteWins +=1
+                whiteWins += 1
                 trainingData = whiteMoves
                 j = 0
                 for count in range(10):
                     trainingData = shuffle(trainingData)
                     while(True):
                         try:
-                            g = backPropError(trainingData[j],w,[1,0])
-                            newW = updateWeights(g,w,trainingData[j])
+                            g = backPropError(trainingData[j], w, [1, 0])
+                            newW = updateWeights(g, w, trainingData[j])
                             w = newW
-                            j+=1
+                            j += 1
                         except IndexError:
                             board.reset()
-                            #board.set_fen('8/8/2k5/5Q2/3K4/8/8/8 w - - 6 4')
                             whiteMoves = []
                             blackMoves = []
                             break
-                        
+
             elif(board.result() == "0-1"):
                 blackWins += 1
                 trainingData = blackMoves
@@ -381,13 +378,12 @@ def train(iterations):
                     shuffle(trainingData)
                     while(True):
                         try:
-                            g = backPropError(trainingData[j],w,[1,0])
-                            newW = updateWeights(g,w,trainingData[j])
+                            g = backPropError(trainingData[j], w, [1, 0])
+                            newW = updateWeights(g, w, trainingData[j])
                             w = newW
-                            j+=1
+                            j += 1
                         except IndexError:
                             board.reset()
-                            #board.set_fen('8/8/2k5/5Q2/3K4/8/8/8 w - - 6 4')
                             whiteMoves = []
                             blackMoves = []
                             break
@@ -398,21 +394,19 @@ def train(iterations):
                     shuffle(trainingData)
                     while(True):
                         try:
-                            g = backPropError(trainingData[j],w,[1,0])
-                            newW = updateWeights(g,w,trainingData[j])
+                            g = backPropError(trainingData[j], w, [1, 0])
+                            newW = updateWeights(g, w, trainingData[j])
                             w = newW
-                            j+=1
+                            j += 1
                         except IndexError:
                             board.reset()
-                            #board.set_fen('8/8/2k5/5Q2/3K4/8/8/8 w - - 6 4')
                             whiteMoves = []
                             blackMoves = []
                             break
-            
-        
+
         m = chooseMove(w)
         m = m[0]
-        if(board.turn == True):
+        if(board.turn):
             playLegalMove(m)
             a = getBoard()
             whiteMoves.append(a)
@@ -425,14 +419,17 @@ def train(iterations):
             b = a - elapsed
             print(str(i) + " took " + str(b))
             elapsed = time.time()
-            
-        i+=1
+
+        i += 1
     saveW(w)
-    print("Games: " + str(games) + "\n" + "White Wins: " + str(whiteWins) + "\n" + "Black Wins: " + str(blackWins))
+    print("Games: " + str(games) + "\n" + "White Wins: " +
+          str(whiteWins) + "\n" + "Black Wins: " + str(blackWins))
+
 
 def play(speed):
+    w = loadW()
     board.reset()
-    while(board.is_game_over() == False):
+    while board.is_game_over() is False:
         m = chooseMove(w)
         m = m[0]
         playLegalMove(m)
@@ -440,77 +437,3 @@ def play(speed):
         print(" ")
         time.sleep(speed)
     print(board.result())
-
-'''
-
- 
-grad_y_pred = 2.0 * (output of forward pass - label)
-    grad_w2 = h_relu.T.dot(grad_y_pred)
-    grad_h_relu = grad_y_pred.dot(w2.T)
-    grad_h = grad_h_relu.copy()
-    grad_h[h < 0] = 0
-    grad_w1 = x.T.dot(grad_h)
-
-
-a[37] = '('
-a = "cunt"
->>> a = a + "bitch"
->>> print(a)
-
-
-
-'''
-
-
-"""
-       
-
-def playMove(f,t):
-    move = chess.square_name(f) + chess.square_name(t)
-    Qmove = chess.square_name(f) + chess.square_name(t) + 'q'
-    isLegal = chess.Move.from_uci(move) in board.legal_moves
-    isQLegal = chess.Move.from_uci(Qmove) in board.legal_moves
-    if(isLegal == True):
-        board.push_uci(move)
-        return True
-    elif(isQLegal == True):
-        board.push_uci(Qmove)
-        return True
-    else:
-        return False
-
-
-
-
-
-
-last_time = time.time()
-i = 0
-inc = 0
-while(i < 64):
-    f = chess.square_name(i)
-    j = 0
-    while(j<64):
-        t = chess.square_name(j)
-        j = j + 1
-        chess.Move.from_uci(f+t) in board.legal_moves
-        inc = inc + 1
-    i = i + 1
-print('loop took {} seconds'.format(time.time()-last_time))
-print(inc)
-"""
-  
-"""
-
-
-board.turn
-#chess.square_name(43)
-#chess.Move.from_uci("a8a1") in board.legal_moves
-
-
-#propogate
-#if not legal just go down the probability list
-#save inputs + output with chosen move set to 1 and all others set to 0
-#play move
-#after one side has won backprop over saved training data for that side
-"""
--- chess-tf.py
+++ chess-tf.py
@@ -1,18 +1,24 @@
 import tensorflow as tf
-import numpy
+import numpy as np
 import chess
 
-#base class for interacting with board
-#TODO add tree searching
+# base class for interacting with board
+# TODO add tree searching
+# TODO create default board parsing
+
+
 class baseBoard(chess.Board):
-    
+
     def __init__(self):
         self.baseBoard = chess.Board()
-        self.parsedBoard = self.baseBoard
+        self.parsedBoard = self.parseBoard()
         self.moveStack = []
         self.trainingData = []
         self.legalMoveList = self.getLegalMoveList()
-        
+
+    def parseBoard(self):
+        self.parsedBoard = self.baseBoard
+
     def getLegalMoveList(self):
         a = list(enumerate(self.baseBoard.legal_moves))
         b = [x[1] for x in a]
@@ -23,40 +29,38 @@ class baseBoard(chess.Board):
             i += 1
         return c
 
-    def parseMove(self,fromSquare,toSquare = None):
+    def parseMove(self, fromSquare, toSquare=None):
         frT = type(fromSquare)
         toT = type(toSquare)
-        if(toSquare == None and frT == str):
+        if(toSquare is None and frT == str):
             move = fromSquare
         elif(frT == str and toT == str):
             move = fromSquare + toSquare
         else:
             return "Invalid Move Representation"
         return move
-    
-    def playMove(self,move):
+
+    def playMove(self, move):
         if(move in self.legalMoveList):
             self.baseBoard.push_uci(move)
         elif(move + 'q' in self.legalMoveList):
             self.baseBoard.push_uci(move + 'q')
         else:
             return 'illegal move'
-        
+
         self.updateAttributes()
         if(self.baseBoard.is_game_over()):
-            return self.baseBoard.result()       
+            return self.baseBoard.result()
 
         return self.parsedBoard
 
-    def addTrainingData(self,move):
-        a = [self.parsedBoard,move]
+    def addTrainingData(self, move):
+        a = [self.parsedBoard, move]
         if(self.playMove(move) != 'illegal move'):
             self.trainingData.append(a)
             return self.parsedBoard
         else:
             return 'illegal move'
-        
-        
 
     def reset(self):
         self.baseBoard.reset()
@@ -65,7 +69,73 @@ class baseBoard(chess.Board):
 
     def updateAttributes(self):
         self.legalMoveList = self.getLegalMoveList()
-        self.parsedBoard = self.baseBoard
-        
-board = baseBoard()
+        self.parseBoard()
 
+# TODO add options to change parsing
+
+
+class parsedBoard(baseBoard):
+
+    def parseBoard(self):
+        b0 = np.zeros((12, 8, 8))
+        i = 0
+        while(i < 8):
+            j = 0
+            while(j < 8):
+                k = str(self.baseBoard.piece_at(i * 8 + j))
+                if(self.baseBoard.turn):
+                    if(k == "P"):
+                        b0[0][i][j] = 1
+                    elif(k == "B"):
+                        b0[1][i][j] = 1
+                    elif(k == "N"):
+                        b0[2][i][j] = 1
+                    elif(k == "R"):
+                        b0[3][i][j] = 1
+                    elif(k == "Q"):
+                        b0[4][i][j] = 1
+                    elif(k == "K"):
+                        b0[5][i][j] = 1
+                    elif(k == "p"):
+                        b0[6][i][j] = 1
+                    elif(k == "b"):
+                        b0[7][i][j] = 1
+                    elif(k == "n"):
+                        b0[8][i][j] = 1
+                    elif(k == "r"):
+                        b0[9][i][j] = 1
+                    elif(k == "q"):
+                        b0[10][i][j] = 1
+                    elif(k == "k"):
+                        b0[11][i][j] = 1
+                else:
+                    if(k == "P"):
+                        b0[6][i][j] = 1
+                    elif(k == "B"):
+                        b0[7][i][j] = 1
+                    elif(k == "N"):
+                        b0[8][i][j] = 1
+                    elif(k == "R"):
+                        b0[9][i][j] = 1
+                    elif(k == "Q"):
+                        b0[10][i][j] = 1
+                    elif(k == "K"):
+                        b0[11][i][j] = 1
+                    elif(k == "p"):
+                        b0[0][i][j] = 1
+                    elif(k == "b"):
+                        b0[1][i][j] = 1
+                    elif(k == "n"):
+                        b0[2][i][j] = 1
+                    elif(k == "r"):
+                        b0[3][i][j] = 1
+                    elif(k == "q"):
+                        b0[4][i][j] = 1
+                    elif(k == "k"):
+                        b0[5][i][j] = 1
+                j = j + 1
+            i = i + 1
+        return b0
+
+
+board = parsedBoard()
